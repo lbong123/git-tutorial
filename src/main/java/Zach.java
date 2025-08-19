02548f95b166ccb9bf1d752e6ce8b5539f1fa2fd
zachr <zach.r0305@gmail.com>
2025-08-19 17:49:09 +1000

    filled in Zach class

--- src/main/java/Zach.java
+++ src/main/java/Zach.java
@@ -1,2 +1,14 @@
 public class Zach {
+    private String name = "Zach";
+
+    public Zach(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Zach{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
